93f76c824c5b45db7809f156a647c1b38bb25224
juwonk1018 <kjon1018@gmail.com>
2023-12-13 17:07:30 +0900

    refactor. 상단 NavBar 주석 처리

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
 
   return (
     <div className={`main ${theme}`}>
-      <NavBar/>
+      {/* <NavBar/> */}
       <StopWatch darkMode = {isDarkMode} setDarkMode = {setIsDarkMode}/>
     </div>
   );
