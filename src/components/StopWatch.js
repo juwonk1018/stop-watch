4848e94053353017936bf5c35b4ed8cd741b912e
juwonk1018 <kjon1018@gmail.com>
2023-12-13 17:41:04 +0900

    design. progressButtons 내부 text 변경

--- src/components/StopWatch.js
+++ src/components/StopWatch.js
@@ -1,5 +1,4 @@
 import React, {useState, useEffect, useRef} from 'react';
-
 import './StopWatch.css';
 
 export default function StopWatch({darkMode, setDarkMode}) {
@@ -8,7 +7,6 @@ export default function StopWatch({darkMode, setDarkMode}) {
     const [visibleButton, setVisibleButton] = useState(true);
     const [progress, setProgress] = useProgress();
     
-
     const darkModeEvent = (e) => {
         setDarkMode(!darkMode);
     }
@@ -95,23 +93,18 @@ export default function StopWatch({darkMode, setDarkMode}) {
                 </div>
 
                 <div className={"progressButtons"}>
-                {visibleButton && !progress && <button onClick={() => setProgress(true)}>
-                    <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M8 5v14l11-7z"/></svg><span>START</span>
-                </button>}
-                {visibleButton && progress && <button onClick={() => setProgress(false)}>
-                    <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z"/></svg><span>STOP</span>
-                </button>}
-                {visibleButton && <button onClick={() => {setTime(0); setProgress(false); }}>
-                    <svg xmlns="http://www.w3.org/2000/svg" enableBackground="new 0 0 24 24" height="24" viewBox="0 0 24 24" width="24"><g><path d="M0,0h24v24H0V0z" fill="none"/></g><g><g><path d="M12,5V2L8,6l4,4V7c3.31,0,6,2.69,6,6c0,2.97-2.17,5.43-5,5.91v2.02c3.95-0.49,7-3.85,7-7.93C20,8.58,16.42,5,12,5z"/><path d="M6,13c0-1.65,0.67-3.15,1.76-4.24L6.34,7.34C4.9,8.79,4,10.79,4,13c0,4.08,3.05,7.44,7,7.93v-2.02 C8.17,18.43,6,15.97,6,13z"/></g></g></svg><span>RESET</span>
-                </button>
-                }
-                
-                
+                    {visibleButton && !progress && <button onClick={() => setProgress(true)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M8 5v14l11-7z"/></svg><span>Start</span>
+                    </button>}
+                    {visibleButton && progress && <button onClick={() => setProgress(false)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z"/></svg><span>Stop</span>
+                    </button>}
+                    {visibleButton && <button onClick={() => {setTime(0); setProgress(false); }}>
+                        <svg xmlns="http://www.w3.org/2000/svg" enableBackground="new 0 0 24 24" height="24" viewBox="0 0 24 24" width="24"><g><path d="M0,0h24v24H0V0z" fill="none"/></g><g><g><path d="M12,5V2L8,6l4,4V7c3.31,0,6,2.69,6,6c0,2.97-2.17,5.43-5,5.91v2.02c3.95-0.49,7-3.85,7-7.93C20,8.58,16.42,5,12,5z"/><path d="M6,13c0-1.65,0.67-3.15,1.76-4.24L6.34,7.34C4.9,8.79,4,10.79,4,13c0,4.08,3.05,7.44,7,7.93v-2.02 C8.17,18.43,6,15.97,6,13z"/></g></g></svg><span>Reset</span>
+                    </button>
+                    }
                 </div>
             </div>
-
-            
-            
         </div>
     )
 }
