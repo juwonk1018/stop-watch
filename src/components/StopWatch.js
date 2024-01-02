62d77261c51227776d4e8ac0663aa384733d0302
juwonk1018 <kjon1018@gmail.com>
2024-01-02 21:42:29 +0900

    design. button span 삭제

--- src/components/StopWatch.js
+++ src/components/StopWatch.js
@@ -14,8 +14,6 @@ export default function StopWatch({darkMode, setDarkMode, recordList, setRecordL
     }
     
     function deleteRecord(recordID) {
-        console.log(recordList, recordID);
-
         const newRecords = recordList.filter((record) => record[0] != recordID);
         setRecordList(newRecords);
         
@@ -69,25 +67,25 @@ export default function StopWatch({darkMode, setDarkMode, recordList, setRecordL
                 <div className="buttonContainer">
                     {visibleButton && <div className={"progressButtons"}>
                         {!progress && <button onClick={() => setProgress(true)}>
-                            <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M8 5v14l11-7z"/></svg><span>Start</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M8 5v14l11-7z"/></svg>
                         </button>}
                         {progress && <button onClick={() => setProgress(false)}>
-                            <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z"/></svg><span>Stop</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z"/></svg>
                         </button>}
                         {<button onClick={() => {setTime(0); prevStopWatch.current = 0; setProgress(false); setRecordList([])}}>
-                            <svg xmlns="http://www.w3.org/2000/svg" enableBackground="new 0 0 24 24" height="24" viewBox="0 0 24 24" width="24"><g><path d="M0,0h24v24H0V0z" fill="none"/></g><g><g><path d="M12,5V2L8,6l4,4V7c3.31,0,6,2.69,6,6c0,2.97-2.17,5.43-5,5.91v2.02c3.95-0.49,7-3.85,7-7.93C20,8.58,16.42,5,12,5z"/><path d="M6,13c0-1.65,0.67-3.15,1.76-4.24L6.34,7.34C4.9,8.79,4,10.79,4,13c0,4.08,3.05,7.44,7,7.93v-2.02 C8.17,18.43,6,15.97,6,13z"/></g></g></svg><span>Reset</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" enableBackground="new 0 0 24 24" height="24" viewBox="0 0 24 24" width="24"><g><path d="M0,0h24v24H0V0z" fill="none"/></g><g><g><path d="M12,5V2L8,6l4,4V7c3.31,0,6,2.69,6,6c0,2.97-2.17,5.43-5,5.91v2.02c3.95-0.49,7-3.85,7-7.93C20,8.58,16.42,5,12,5z"/><path d="M6,13c0-1.65,0.67-3.15,1.76-4.24L6.34,7.34C4.9,8.79,4,10.79,4,13c0,4.08,3.05,7.44,7,7.93v-2.02 C8.17,18.43,6,15.97,6,13z"/></g></g></svg>
                         </button>
                         }
                         <button onClick={() => {setRecordList((prev) => [...prev, [recordID.current, timeHMS(time-prevStopWatch.current), timeHMS(time)]]); prevStopWatch.current = time; recordID.current += 1;}}>
-                            <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M8 5v14l11-7z"/></svg><span>REC</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" height="20" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M7.88 3.39L6.6 1.86 2 5.71l1.29 1.53 4.59-3.85zM22 5.72l-4.6-3.86-1.29 1.53 4.6 3.86L22 5.72zM12 4c-4.97 0-9 4.03-9 9s4.02 9 9 9c4.97 0 9-4.03 9-9s-4.03-9-9-9zm0 16c-3.87 0-7-3.13-7-7s3.13-7 7-7 7 3.13 7 7-3.13 7-7 7zm1-11h-2v3H8v2h3v3h2v-3h3v-2h-3V9z"/></svg>
                         </button>
                     </div>
                     }
                     <div className="emptyDiv"></div>
                 </div>
                 
-                
-                <div className = {"recordList " + (visibleButton ? '' : 'recordList-hide')}>
+                {
+                    <div className = {"recordList " + (visibleButton ? '' : 'recordList-hide')}>
                     <table>
                         <thead>
                             <th class = {(darkMode ? 'dark' : 'light')}>No.</th>
@@ -104,7 +102,7 @@ export default function StopWatch({darkMode, setDarkMode, recordList, setRecordL
                                     <td>{item[2]}</td>
                                     <td>
                                         <button class = {(darkMode ? 'dark' : 'light')} id = "deleteRecordBtn" onClick = {() => deleteRecord(item[0])}>
-                                            ㅡ
+                                            ─
                                         </button>
                                     </td>
                                 </tr>  
@@ -112,6 +110,8 @@ export default function StopWatch({darkMode, setDarkMode, recordList, setRecordL
                         </tbody>
                     </table>
                 </div>
+                }
+                
                 
             </div>
         </div>
