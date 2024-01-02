53c58300e4b3290774ec8485ba7f3318168a3aea
juwonk1018 <kjon1018@gmail.com>
2024-01-02 12:05:16 +0900

    feat. recordList 스크롤 추가

--- src/components/StopWatch.js
+++ src/components/StopWatch.js
@@ -68,7 +68,7 @@ export default function StopWatch({darkMode, setDarkMode, recordList, setRecordL
                             <svg xmlns="http://www.w3.org/2000/svg" enableBackground="new 0 0 24 24" height="24" viewBox="0 0 24 24" width="24"><g><path d="M0,0h24v24H0V0z" fill="none"/></g><g><g><path d="M12,5V2L8,6l4,4V7c3.31,0,6,2.69,6,6c0,2.97-2.17,5.43-5,5.91v2.02c3.95-0.49,7-3.85,7-7.93C20,8.58,16.42,5,12,5z"/><path d="M6,13c0-1.65,0.67-3.15,1.76-4.24L6.34,7.34C4.9,8.79,4,10.79,4,13c0,4.08,3.05,7.44,7,7.93v-2.02 C8.17,18.43,6,15.97,6,13z"/></g></g></svg><span>Reset</span>
                         </button>
                         }
-                        <button onClick={() => {setRecordList((prev) => [...prev, [timeHMS(time-prevStopWatch.current), timeHMS(time)]]); prevStopWatch.current = time}}>
+                        <button onClick={() => {setRecordList((prev) => [...prev, [timeHMS(time-prevStopWatch.current), timeHMS(time)]]); prevStopWatch.current = time; console.log(recordList)}}>
                             <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M8 5v14l11-7z"/></svg><span>REC</span>
                         </button>
                     </div>
@@ -79,16 +79,24 @@ export default function StopWatch({darkMode, setDarkMode, recordList, setRecordL
                 
                 <div className = {"recordList " + (visibleButton ? '' : 'recordList-hide')}>
                     <table>
-                        <th>No.</th>
-                        <th>LAB TIME</th>
-                        <th>TOTAL TIME</th>
-                        {recordList.map((item, idx) =>(
-                            <tr>
-                                <td>{idx+1}</td>
-                                <td>{item[0]}</td>
-                                <td>{item[1]}</td>
-                            </tr>  
-                        ))}
+                        <thead>
+                            <th>No.</th>
+                            <th>LAB TIME</th>
+                            <th>TOTAL</th>
+                            <th></th>
+                        </thead>
+                        
+                        <tbody>
+                            {recordList.map((item, idx) =>(
+                                <tr key={idx+1}>
+                                    <td>{idx+1}</td>
+                                    <td>{item[0]}</td>
+                                    <td>{item[1]}</td>
+                                    <td>
+                                    </td>
+                                </tr>  
+                            ))}
+                        </tbody>
                     </table>
                 </div>
                 
